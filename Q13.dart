16703ee23757d70da3a144fb9aa5c74ec65b827f
Leyva Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2023-07-18 14:55:34 -0300

    Create Q13.dart

---
+++ Q13.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+main() {
+  int fat;
+  print("Insira um valor para o qual deseja calcular seu fatorial: ");
+  int n = int.parse(stdin.readLineSync()!);
+  for (fat = 1; n > 1; n = n - 1) fat = fat * n;
+  print("\nFatorial calculado:  $fat");
+}
