3fe22035913cf20c908bb5a87560b4ca6e863379
Leyva Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2023-07-19 13:49:23 -0300

    update

--- Q7.dart
+++ Q7.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 main() {
-  print("Entre com dois valores");
+  print('Entre com dois valores');
 
   int a = int.parse(stdin.readLineSync()!);
   int b = int.parse(stdin.readLineSync()!);
